d87a2f94bbb166f1b03cf6d4c06e8386200c32d9
mousumigithub <nandimousumi470@gmail.com>
2022-02-25 20:12:55 +0530

    1st commit

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+
+
+Vue.createApp({ 
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+
+    }
+}).mount('#app');
+
+
+
+
+
+
+
+
+
+
+
+
+
